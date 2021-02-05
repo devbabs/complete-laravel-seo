63b72671c633af58c4171b3ad3d2a5866bcfab3a
Babalola Macaulay <babsmacheda@ymail.com>
2021-02-05 13:25:12 +0100

    Initial Commit

--- src/routes.php
+++ src/routes.php
@@ -6,7 +6,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('seo-dashboard', [
         'as' => 'laravel-seo.dashboard',
         'uses' => function () {
-            return "Welcome to the LaravelSEO Complete Stable Version!";
+            return "Welcome to the LaravelSEO Complete Stable Version 1.0.1!";
         }
     ]);
 });
